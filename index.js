6a91ff2d412419df00d95091ce6a70b58ffb657c
Sarah F <sarahelayna@gmail.com>
2018-01-13 22:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('HELLO'.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {
+    return "I can't hear you!"; // true, returns if lowercase
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!"; // true, returns if uppercase
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."; // false, mixed case
+    }
+}
